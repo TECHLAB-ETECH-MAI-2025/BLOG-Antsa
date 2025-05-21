028a83d553412a7e406afb03357a74d787cf9559
Astna17 <hei.antsa.17@gmail.com>
2025-05-22 02:21:27 +0300

    feat: paginate list of categories

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,16 +10,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/category')]
 final class CategoryController extends AbstractController
 {
     #[Route(name: 'app_category_index', methods: ['GET'])]
-    public function index(CategoryRepository $categoryRepository): Response
+    public function index(CategoryRepository $categoryRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        return $this->render('category/index.html.twig', [
-            'categories' => $categoryRepository->findAll(),
-        ]);
+    $pagination = $paginator->paginate(
+        $categoryRepository->findAll(),       
+        $request->query->getInt('page', 1),   
+        2                                   
+    );
+
+    return $this->render('category/index.html.twig', [
+        'categories' => $pagination,
+    ]);
     }
 
     #[Route('/new', name: 'app_category_new', methods: ['GET', 'POST'])]
